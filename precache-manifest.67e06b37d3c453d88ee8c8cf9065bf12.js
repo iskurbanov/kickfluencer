b9ee73f305501091f66f72b1bd9f4de7cf5330f4
Iskandar Kurbanov <iskurbanov@gmail.com>
2019-05-25 14:30:28 -0400

    Updates

---
+++ precache-manifest.67e06b37d3c453d88ee8c8cf9065bf12.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bddd2e1e7fca9273124b4ad6e738fd8",
+    "url": "/kickfluencer/index.html"
+  },
+  {
+    "revision": "777ddede3d535ceb2e68",
+    "url": "/kickfluencer/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "c2d5f667ad930203e9d7",
+    "url": "/kickfluencer/static/css/main.9de41e44.chunk.css"
+  },
+  {
+    "revision": "777ddede3d535ceb2e68",
+    "url": "/kickfluencer/static/js/2.77e10432.chunk.js"
+  },
+  {
+    "revision": "c2d5f667ad930203e9d7",
+    "url": "/kickfluencer/static/js/main.8eec9bde.chunk.js"
+  },
+  {
+    "revision": "9e1f209163b1fb413fea",
+    "url": "/kickfluencer/static/js/runtime~main.d145c2be.js"
+  },
+  {
+    "revision": "222586f42fa280e476462914cdf70dec",
+    "url": "/kickfluencer/static/media/AvenirNextLTPro-Bold.222586f4.otf"
+  },
+  {
+    "revision": "1a1c2c769bb1a94529f507e96d6985ac",
+    "url": "/kickfluencer/static/media/AvenirNextLTPro-Demi.1a1c2c76.otf"
+  },
+  {
+    "revision": "67016125d8abf229098a0195c7f9ed64",
+    "url": "/kickfluencer/static/media/AvenirNextLTPro-Regular.67016125.otf"
+  },
+  {
+    "revision": "ff37396e6f1513604b505cc8f71f1f6f",
+    "url": "/kickfluencer/static/media/Avg backers (1).ff37396e.png"
+  },
+  {
+    "revision": "aebf4f406b32db1b0de31624fce66db6",
+    "url": "/kickfluencer/static/media/Avgraised (1).aebf4f40.png"
+  },
+  {
+    "revision": "bcc860828ffbc19bd833db467d32ba26",
+    "url": "/kickfluencer/static/media/Frankfurter Std Regular.bcc86082.otf"
+  },
+  {
+    "revision": "c0c229f908a9eb6e1fc0c91039dddb79",
+    "url": "/kickfluencer/static/media/Poppins-Black.c0c229f9.ttf"
+  },
+  {
+    "revision": "5bb3128376ff7ca534d894e470f1c2e9",
+    "url": "/kickfluencer/static/media/Poppins-SemiBold.5bb31283.ttf"
+  },
+  {
+    "revision": "cdbd1b4d1131b7c59514280c3e0a505a",
+    "url": "/kickfluencer/static/media/fashion success proportion.cdbd1b4d.png"
+  },
+  {
+    "revision": "f2b6813a4e1b90c5eb8bbf7a2551058e",
+    "url": "/kickfluencer/static/media/hero-image@3x.f2b6813a.jpg"
+  },
+  {
+    "revision": "0102ea24e2b9746ebe9725f8c86d8ca2",
+    "url": "/kickfluencer/static/media/isaac-smith-1182056-unsplash@3x.0102ea24.jpg"
+  },
+  {
+    "revision": "322606b468309c96c807365fd73a5f1d",
+    "url": "/kickfluencer/static/media/kickfleuncer_logo.322606b4.png"
+  },
+  {
+    "revision": "bdde3387eb09033fa3acfcc2ce1a5012",
+    "url": "/kickfluencer/static/media/maddi-bazzocco-1178337-unsplash@3x.bdde3387.jpg"
+  },
+  {
+    "revision": "fc03e3fca00923aa4acc2beeea47086b",
+    "url": "/kickfluencer/static/media/most successful sub category.fc03e3fc.png"
+  },
+  {
+    "revision": "12cbbf846d67e314f8a5c58380a34431",
+    "url": "/kickfluencer/static/media/thought-catalog-214785-unsplash@3x.12cbbf84.jpg"
+  }
+]);
